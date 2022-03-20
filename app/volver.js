290b807fc73315dd407f65d80e30e8cad5998351
Frank Sera <78000380+seracreativo@users.noreply.github.com>
2022-03-20 18:53:58 -0400

    Update volver.js

--- app/volver.js
+++ app/volver.js
@@ -1,3 +1,3 @@
 var nav = document.getElementById("nav");
 let volver = "← volver";
-nav.innerHTML = `<p><a href="/index.html">${volver}</a></p>`;
+nav.innerHTML = `<p><a href="https://seracreativo.github.io/">${volver}</a></p>`;
